f55eb4d2737b509aa0f824ca13fdc2719043d3dc
Álvaro Lopes Rios <contatoalvarorios@gmail.com>
2024-11-20 19:57:32 -0500

    Add files via upload

---
+++ estudo/views.py
@@ -0,0 +1,10 @@
+from estudo import app
+from flask import render_template
+
+@app.route('/')
+def homepage():
+    return render_template('index.html')
+
+@app.route('/segundapagina/')
+def segundapagina():
+    return render_template('segundapagina.html')
