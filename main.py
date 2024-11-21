cefa9f22cc1e84b20bf712de7d60d6e66926cc0c
Álvaro Lopes Rios <contatoalvarorios@gmail.com>
2024-11-20 19:59:22 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+from estudo import app
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+'''
 from flask import Flask
 
 app = Flask(__name__)
@@ -8,3 +16,5 @@ def homepage():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+'''
